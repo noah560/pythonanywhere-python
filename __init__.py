a3493968e8ce3f2027f89db6e48c4942bc4f01d0
noah560 <64210682+noah560@users.noreply.github.com>
2023-06-21 16:29:26 -0500

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -6,13 +6,19 @@ def setup(usernameIn, tokenIn):
     username, token = usernameIn, tokenIn
 
 
-def api_request(path, **data):
+def api_request(path, method, **data):
     global username, token
-    response = requests.post(
-    'https://www.pythonanywhere.com' + path.format(
-        username=username,
-        id=id
-    ),
+    if method=="post":
+        run = requests.post
+    elif method=="get":
+        run = requests.get
+    elif method=="put":
+        run = requests.put
+    elif method=="delete":
+        run = requests.delete
+    response = run(
+    'https://www.pythonanywhere.com' + path.replace("[username]", username),
     headers={'Authorization': 'Token {token}'.format(token=token)},
     data=data
     )
+    return response
